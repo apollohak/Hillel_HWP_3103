417dbf5401bac8b05ed4b3c6551387a5fa72e1eb
apollohak <apollobiohak@gmail.com>
2021-05-28 19:47:45 +0300

    refactor hmw 13

--- Homework_13/HomeWork_13.py
+++ Homework_13/HomeWork_13.py
@@ -1,34 +1,36 @@
-##################################################
 
 
 class FileReader:
     def __init__(self, filename):
         self.filename = filename
 
-    def read_file(self):
+    def _read_file(self):
         with open(self.filename, "r") as txt_file:
             data = txt_file.readlines()
         return data
 
 
+##################################################
+
 """
-1. Написать функцию, которая получает в виде параметра имя файла названия интернет доменов (domains.txt)
+1. Написать класс, который получает в виде параметра имя файла названия интернет доменов (domains.txt)
 и возвращает их в виде списка строк (названия возвращать без точки).
 """
 
 
 class DomainsWorker(FileReader):
-    def create_domains_list(self):
-        res_list = [name.replace(".", "")[:-1] for name in self.read_file()]
+    def _create_domains_list(self):
+        res_list = [name.replace(".", "")[:-1] for name in self._read_file()]
         return res_list
 
     def __repr__(self):
-        return f"{self.create_domains_list()}"
+        return f"{self._create_domains_list()}"
 
 
 ##################################################
+
 """
-2. Написать функцию, которая получает в виде параметра имя файла (names.txt)
+2. Написать класс, который получает в виде параметра имя файла (names.txt)
 и возвращает список всех фамилий из него.
 Каждая строка файла содержит номер, фамилию, страну, некоторое число (таблица взята с википедии).
 Разделитель - символ табуляции "\t"
@@ -36,18 +38,18 @@ class DomainsWorker(FileReader):
 
 
 class NamesWorker(FileReader):
-    def create_surname_list(self):
-        res_list = [surname.split("\t")[1] for surname in self.read_file()]
+    def _create_surname_list(self):
+        res_list = [surname.split("\t")[1] for surname in self._read_file()]
         return res_list
 
     def __repr__(self):
-        return f"{self.create_surname_list()}"
+        return f"{self._create_surname_list()}"
 
 
 ##################################################
 
 """
-3. Написать функцию, которая получает в виде параметра имя файла (authors.txt) и возвращает список
+3. Написать класс, который получает в виде параметра имя файла (authors.txt) и возвращает список
 словарей вида {"date_original": date_original, "date_modified": date_modified}
 в которых date_original - это дата из строки (если есть),
 а date_modified - эта же дата, представленная в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
@@ -70,20 +72,21 @@ class ModDateWorker(FileReader):
                        'October': '10',
                        'November': '11',
                        'December': '12'}
-        self.create_modified_dictionaries()
+        self.split_data = self._create_split_date_list()
+        self._create_modified_dictionaries()
 
-    def create_split_date_list(self):
+    def _create_split_date_list(self):
         result = []
-        for data in self.read_file():
+        for data in self._read_file():
             data = data.split("-")[0]
             data = data.split()
             if len(data) == 3:
                 result.append(data)
         return result
 
-    def create_modified_dictionaries(self):
+    def _create_modified_dictionaries(self):
         result = []
-        for data in self.create_split_date_list():
+        for data in self.split_data:
             data_dd = data[0]
             data_dd = data_dd[:-2]
             if len(data_dd) == 1:
@@ -93,12 +96,12 @@ class ModDateWorker(FileReader):
         return result
 
     def __repr__(self):
-        return f"{self.create_modified_dictionaries()}"
+        return f"{self._create_modified_dictionaries()}"
 
 
+domains_worker = DomainsWorker("domains.txt")
 surnames = NamesWorker("names.txt")
 modified_date = ModDateWorker("authors.txt")
-domains_worker = DomainsWorker("domains.txt")
 print(domains_worker)
 print(surnames)
 print(modified_date)
