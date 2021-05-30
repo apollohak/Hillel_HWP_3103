1e814bb7eea9afa004094859fc544b8141dd3a38
apollohak <apollobiohak@gmail.com>
2021-05-30 12:23:25 +0300

    add hmw 12 dict, list, zip

---
+++ Homework_12/HomeWork_12_dict.py
@@ -0,0 +1,53 @@
+from random import randint
+import requests
+import csv
+
+############################################
+
+"""
+1. Написать функцию, которая принимает в виде параметра целое число - количество цитат.
+и возвращает список не повторяющихся цитат. Если автор не указан, цитату не брать.
+"""
+
+
+def create_quotes_list(count):
+    quotes = []
+    res = []
+    while len(quotes) < count:
+        params = {"method": "getQuote",
+                  "format": "json",
+                  "key": randint(0, 999)}
+        r = requests.get(url, params=params)
+        data = r.json()
+        author = data["quoteAuthor"]
+        quote = data["quoteText"]
+        link = data["quoteLink"]
+        if author and quote not in quotes:
+            quotes.append(quote)
+            res.append(dict(Author=author, Quote=quote, URL=link))
+    return res
+
+
+############################################
+
+"""
+2. Написать функцию, которая принимает результат предыдущей функции и сохраняет в csv файл.
+Имя файла сделать параметром по умолчанию.
+Заголовки csv файла:
+Author, Quote, URL.
+Перед сохранением в csv, записи отсортировать по автору (в алфавитном порядке).
+Разделитель - запятая.
+"""
+
+
+def create_quotes_csv_file(data):
+    with open("quote_dict.csv", 'w', encoding='windows-1251') as f:
+        header = ["Author", "Quote", "URL"]
+        writer = csv.DictWriter(f, delimiter=",", lineterminator="\r", fieldnames=header)
+        writer.writeheader()
+        writer.writerows(sorted(data, key=lambda x: x.get("Author")))
+
+
+url = "http://api.forismatic.com/api/1.0/"
+quotes_list = create_quotes_list(10)
+create_quotes_csv_file(quotes_list)
